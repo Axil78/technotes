81a8385c50806083aef8516c2147bf3785a1cc32
Axil78 <142527230+Axil78@users.noreply.github.com>
2024-11-20 13:38:41 +0530

    Update Public.js

--- src/components/Public.js
+++ src/components/Public.js
@@ -5,18 +5,18 @@ const Public = () => {
     const content = (
         <section className="public">
             <header>
-                <h1>Welcome to <span className="nowrap">Dan D. Repairs!</span></h1>
+                <h1>Welcome to <span className="nowrap">Axil Repairs!</span></h1>
             </header>
             <main className="public__main">
-                <p>Located in Beautiful Downtown Foo City, Dan D. Repairs  provides a trained staff ready to meet your tech repair needs.</p>
+                <p>Located in Amreli City.</p>
                 <address className="public__addr">
-                    Dan D. Repairs<br />
-                    555 Foo Drive<br />
-                    Foo City, CA 12345<br />
-                    <a href="tel:+15555555555">(555) 555-5555</a>
+                   Axil Repairs<br />
+                    Amreli,Gujarat<br />
+                    Pincode:382421<br />
+                    
                 </address>
                 <br />
-                <p>Owner: Dan Davidson</p>
+                <p>Owner: Axil Nakrani</p>
             </main>
             <footer>
                 <Link to="/login">Employee Login</Link>
